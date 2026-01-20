d3988b11511cf089ee0e33f86bfeb99c415be8c0
surtexx <robert-mihai.gheorghe@s.unibuc.ro>
2026-01-20 16:37:03 +0200

    Complete project

--- solutions.sql
+++ solutions.sql
@@ -9,22 +9,21 @@ ADMINISTER KEY MANAGEMENT SET KEYSTORE OPEN IDENTIFIED BY MyPassword; -- ran fro
 SELECT STATUS FROM V$ENCRYPTION_WALLET;
 ADMINISTER KEY MANAGEMENT SET KEY FORCE KEYSTORE IDENTIFIED BY MyPassword WITH BACKUP USING 'emp_key_backup'; -- ran from cmd as sysdba
 ALTER TABLE SPONSOR MODIFY (VENIT_ANUAL NUMBER ENCRYPT);
-SELECT *
+SELECT ENCRYPTION_ALG
 FROM dba_encrypted_columns
 WHERE table_name = 'SPONSOR';
 ALTER SESSION SET CONTAINER = dbsecdb2;
 
 -- 3. Database activity Auditing
 --  a. Standard Auditing
-
 AUDIT SESSION;
 
 AUDIT INSERT, DELETE ON PARTIDA BY ACCESS;
 
 -- Test
-INSERT INTO PARTIDA
-VALUES(177, 176, 160, 27, 4, 3, 48519);
+INSERT INTO PARTIDA VALUES(177, 176, 160, 27, 4, 3, 48519);
 DELETE FROM PARTIDA WHERE id_partida=177;
+select COUNT(*) from dba_audit_trail where obj_name = 'PARTIDA';
 
 --  b.	Audit Triggers
 
@@ -74,18 +73,33 @@ DELETE FROM ECHIPA WHERE id_echipa=177;
 SELECT * FROM echipa_audit_log;
 
 --  c.	Audit Policies
-CREATE AUDIT POLICY campionat_policy
-  ACTIONS INSERT, UPDATE, DELETE ON CAMPIONAT;
 
-AUDIT POLICY campionat_policy BY PUBLIC;
+create or replace procedure proc_audit as
+begin
+    dbms_fga.add_policy(
+        object_schema => 'SYSTEM',
+        object_name => 'CAMPIONAT',
+        policy_name => 'campionat_policy',
+        enable => false,
+        statement_types => 'INSERT,UPDATE'
+    );
+end;
+/
+execute proc_audit;
+begin
+    dbms_fga.enable_policy(
+        object_schema => 'SYSTEM',
+        object_name => 'CAMPIONAT',
+        policy_name => 'campionat_policy'
+    );
+end;
+/
 
 -- Test
-
-INSERT INTO CAMPIONAT(id_campionat, tara, an_infiintare_campionat, rang) VALUES (8, 'test', 1999, 1);
-DELETE FROM CAMPIONAT where ID_CAMPIONAT = 8;
-SELECT *
-FROM UNIFIED_AUDIT_TRAIL
-WHERE object_name = 'CAMPIONAT';
+INSERT INTO SYSTEM.CAMPIONAT(id_campionat, tara, an_infiintare_campionat, rang) VALUES (8, 'test', 1999, 1); -- ca alt user decat system (test_adm)
+DELETE FROM SYSTEM.CAMPIONAT where ID_CAMPIONAT = 8; -- ca alt user decat system (test_adm)
+select db_user, userhost, policy_name, timestamp, sql_text
+from dba_fga_audit_trail where db_user = 'TEST_ADM';
 
 -- 4. Management of Database Users and Computational Resources
 --  b. Implementing the identity management configuration in the database
@@ -120,94 +134,89 @@ ALTER USER mod3 profile profil_moderator;
 
 -- 5. Privileges and Roles
 
-CREATE ROLE C##admin_campionat;
-CREATE ROLE C##admin_echipa;
-CREATE ROLE C##admin_jucator;
-CREATE ROLE C##moderator_campionat;
-CREATE ROLE C##moderator_echipa;
-CREATE ROLE C##moderator_jucator;
-CREATE ROLE C##readonly_user;
+CREATE ROLE admin_campionat;
+CREATE ROLE admin_echipa;
+CREATE ROLE admin_jucator;
+CREATE ROLE moderator_campionat;
+CREATE ROLE moderator_echipa;
+CREATE ROLE moderator_jucator;
+CREATE ROLE readonly_user;
 
-GRANT CREATE SESSION TO C##admin_campionat;
-GRANT CREATE SESSION TO C##admin_echipa;
-GRANT CREATE SESSION TO C##admin_jucator;
-GRANT CREATE SESSION TO C##moderator_campionat;
-GRANT CREATE SESSION TO C##moderator_echipa;
-GRANT CREATE SESSION TO C##moderator_jucator;
-GRANT CREATE SESSION TO C##readonly_user;
+GRANT CREATE SESSION TO admin_campionat;
+GRANT CREATE SESSION TO admin_echipa;
+GRANT CREATE SESSION TO admin_jucator;
+GRANT CREATE SESSION TO moderator_campionat;
+GRANT CREATE SESSION TO moderator_echipa;
+GRANT CREATE SESSION TO moderator_jucator;
+GRANT CREATE SESSION TO readonly_user;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON CAMPIONAT TO C##admin_campionat;
-GRANT SELECT, INSERT, UPDATE, DELETE ON SPONSOR TO C##admin_campionat;
-GRANT SELECT, INSERT, UPDATE, DELETE ON STADION TO C##admin_campionat;
-GRANT SELECT, INSERT, UPDATE, DELETE ON DIVIZIE TO C##admin_campionat;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ARBITRU TO C##admin_campionat;
-GRANT SELECT, INSERT, UPDATE, DELETE ON PARTIDA TO C##admin_campionat;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ARBITRU_PARTIDA TO C##admin_campionat; -- example of c.	Privileges on dependent objects
-GRANT SELECT, INSERT, UPDATE, DELETE ON ECHIPA TO C##admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.CAMPIONAT TO admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.SPONSOR TO admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.STADION TO admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.DIVIZIE TO admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.ARBITRU TO admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.PARTIDA TO admin_campionat;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.ARBITRU_PARTIDA TO admin_campionat; -- example of c.	Privileges on dependent objects
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.ECHIPA TO admin_campionat;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ECHIPA TO C##admin_echipa;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ANTRENOR TO C##admin_echipa;
-GRANT SELECT, INSERT, UPDATE, DELETE ON LICENTA TO C##admin_echipa;
-GRANT SELECT, INSERT, UPDATE, DELETE ON JUCATOR TO C##admin_echipa;
-GRANT SELECT, INSERT, UPDATE, DELETE ON STATISTICI_INDIVIDUALE TO C##admin_echipa;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ECHIPA TO admin_echipa;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.ANTRENOR TO admin_echipa;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.LICENTA TO admin_echipa;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.JUCATOR TO admin_echipa;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.STATISTICI_INDIVIDUALE TO admin_echipa;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON JUCATOR TO C##admin_jucator;
-GRANT SELECT, INSERT, UPDATE, DELETE ON STATISTICI_INDIVIDUALE TO C##admin_jucator;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.JUCATOR TO admin_jucator;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SYSTEM.STATISTICI_INDIVIDUALE TO admin_jucator;
 
-GRANT SELECT, INSERT, UPDATE ON CAMPIONAT TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON SPONSOR TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON STADION TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON DIVIZIE TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON ARBITRU TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON PARTIDA TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON ARBITRU_PARTIDA TO C##moderator_campionat;
-GRANT SELECT, INSERT, UPDATE ON ECHIPA TO C##moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.CAMPIONAT TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.SPONSOR TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.STADION TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.DIVIZIE TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.ARBITRU TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.PARTIDA TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.ARBITRU_PARTIDA TO moderator_campionat;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.ECHIPA TO moderator_campionat;
 
-GRANT SELECT, INSERT, UPDATE ON ECHIPA TO C##moderator_echipa;
-GRANT SELECT, INSERT, UPDATE ON ANTRENOR TO C##moderator_echipa;
-GRANT SELECT, INSERT, UPDATE ON LICENTA TO C##moderator_echipa;
-GRANT SELECT, INSERT, UPDATE ON JUCATOR TO C##moderator_echipa;
-GRANT SELECT, INSERT, UPDATE ON STATISTICI_INDIVIDUALE TO C##moderator_echipa;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.ECHIPA TO moderator_echipa;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.ANTRENOR TO moderator_echipa;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.LICENTA TO moderator_echipa;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.JUCATOR TO moderator_echipa;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.STATISTICI_INDIVIDUALE TO moderator_echipa;
 
-GRANT SELECT, INSERT, UPDATE ON JUCATOR TO C##moderator_jucator;
-GRANT SELECT, INSERT, UPDATE ON STATISTICI_INDIVIDUALE TO C##moderator_jucator;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.JUCATOR TO moderator_jucator;
+GRANT SELECT, INSERT, UPDATE ON SYSTEM.STATISTICI_INDIVIDUALE TO moderator_jucator;
 
-GRANT SELECT ON CAMPIONAT TO C##readonly_user;
-GRANT SELECT ON SPONSOR TO C##readonly_user;
-GRANT SELECT ON STADION TO C##readonly_user;
-GRANT SELECT ON DIVIZIE TO C##readonly_user;
-GRANT SELECT ON ARBITRU TO C##readonly_user;
-GRANT SELECT ON PARTIDA TO C##readonly_user;
-GRANT SELECT ON ARBITRU_PARTIDA TO C##readonly_user;
-GRANT SELECT ON ECHIPA TO C##readonly_user;
-GRANT SELECT ON ANTRENOR TO C##readonly_user;
-GRANT SELECT ON LICENTA TO C##readonly_user;
-GRANT SELECT ON JUCATOR TO C##readonly_user;
-GRANT SELECT ON STATISTICI_INDIVIDUALE TO C##readonly_user;
+GRANT SELECT ON SYSTEM.CAMPIONAT TO readonly_user;
+GRANT SELECT ON SYSTEM.SPONSOR TO readonly_user;
+GRANT SELECT ON SYSTEM.STADION TO readonly_user;
+GRANT SELECT ON SYSTEM.DIVIZIE TO readonly_user;
+GRANT SELECT ON SYSTEM.ARBITRU TO readonly_user;
+GRANT SELECT ON SYSTEM.PARTIDA TO readonly_user;
+GRANT SELECT ON SYSTEM.ARBITRU_PARTIDA TO readonly_user;
+GRANT SELECT ON SYSTEM.ECHIPA TO readonly_user;
+GRANT SELECT ON SYSTEM.ANTRENOR TO readonly_user;
+GRANT SELECT ON SYSTEM.LICENTA TO readonly_user;
+GRANT SELECT ON SYSTEM.JUCATOR TO readonly_user;
+GRANT SELECT ON SYSTEM.STATISTICI_INDIVIDUALE TO readonly_user;
 
-GRANT C##readonly_user TO C##moderator_jucator; -- example of b. Privileges hierarchies
-GRANT C##readonly_user TO C##moderator_echipa;
-GRANT C##readonly_user TO C##moderator_campionat;
+GRANT readonly_user TO moderator_jucator; -- example of b. Privileges hierarchies
+GRANT readonly_user TO moderator_echipa;
+GRANT readonly_user TO moderator_campionat;
 
-GRANT C##moderator_jucator TO C##admin_jucator;
-GRANT C##moderator_echipa TO C##admin_echipa;
-GRANT C##moderator_campionat TO C##admin_campionat;
+GRANT moderator_jucator TO admin_jucator;
+GRANT moderator_echipa TO admin_echipa;
+GRANT moderator_campionat TO admin_campionat;
 
-GRANT C##admin_campionat TO adm1;
-GRANT C##admin_echipa TO adm2;
-GRANT C##admin_jucator TO adm3;
-GRANT C##moderator_campionat TO mod1;
-GRANT C##moderator_echipa TO mod2;
-GRANT C##moderator_jucator TO mod3;
+GRANT admin_campionat TO adm1;
+GRANT admin_echipa TO adm2;
+GRANT admin_jucator TO adm3;
+GRANT moderator_campionat TO mod1;
+GRANT moderator_echipa TO mod2;
+GRANT moderator_jucator TO mod3
 
 -- Test
-SELECT * 
-FROM dba_tab_privs
-WHERE grantee IN (
-  'C##READONLY_USER',
-  'C##MODERATOR_JUCATOR',
-  'C##ADMIN_JUCATOR'
-);
+INSERT INTO SYSTEM.JUCATOR VALUES(10000, 'Ederson Moraes', 1, 'portar', 1, 'drept'); -- as mod3
+DELETE FROM SYSTEM.JUCATOR where id_jucator = 10000 -- won't work as mod3, but as adm3 yes
 
 -- 6. Database Applications and Data Security
 --  b. SQL Injection
@@ -252,12 +261,12 @@ END;
 -- 7. Data masking
 
 CREATE OR REPLACE DIRECTORY masking_dir AS 'C:\Users\rober\Desktop\oracle19c\admin\mask';
-GRANT READ, WRITE ON directory masking_dir TO C##admin_campionat;
-GRANT READ, WRITE ON directory masking_dir TO C##admin_echipa;
-GRANT READ, WRITE ON directory masking_dir TO C##admin_jucator;
-GRANT READ, WRITE ON directory masking_dir TO C##moderator_campionat;
+GRANT READ, WRITE ON directory masking_dir TO admin_campionat;
+GRANT READ, WRITE ON directory masking_dir TO admin_echipa;
+GRANT READ, WRITE ON directory masking_dir TO admin_jucator;
+GRANT READ, WRITE ON directory masking_dir TO moderator_campionat;
 GRANT READ, WRITE ON directory masking_dir TO C#moderator_echipa;
-GRANT READ, WRITE ON directory masking_dir TO C##moderator_jucator;
+GRANT READ, WRITE ON directory masking_dir TO moderator_jucator;
 
 CREATE OR REPLACE PACKAGE data_masking is
     FUNCTION masking_nume_sponsor(nume VARCHAR2) return VARCHAR2;
